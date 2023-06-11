b5b73a57b37597efa2340994ca5fff7ab15dfbd6
zain ul rab <zayn.rab604@gmail.com>
2023-06-11 12:55:12 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '659e5d3091msh4120861d19b087bp152d88jsn1732405544d5',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city) => {
+	cityName.innerHTML = city
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, options)
+		.then(response => response.json())
+		.then(response =>{ 
+			console.log(response)
+
+			cloud_pct.innerHTML  = response.cloud_pct
+			cloud_pct2.innerHTML  = response.cloud_pct
+			temp.innerHTML  = response.temp
+			//feels_like.innerHTML  = response.feels_like
+			humidity.innerHTML  = response.humidity
+			humidity2.innerHTML  = response.humidity
+			min_temp.innerHTML  = response.min_temp
+			temp2.innerHTML  = response.max_temp
+			max_temp.innerHTML  = response.max_temp
+			wind_speed.innerHTML  = response.wind_speed
+			wind_degrees.innerHTML  = response.wind_degrees
+			sunrise.innerHTML  = response.sunrise
+			sunset.innerHTML  = response.sunset
+		})
+		.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+
+getWeather("Karachi")
